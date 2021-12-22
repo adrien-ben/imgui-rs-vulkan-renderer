62eaa3c32db431012806d604ee7ede297b26907a
Adrien Bennadji <adrien.bennadji@live.fr>
2021-12-22 14:58:54 +0100

    use thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,57 +1,23 @@
 use ash::vk;
 use imgui::TextureId;
-use std::{error::Error, fmt, io};
+use thiserror::Error;
 
 /// Crates error type.
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum RendererError {
     /// Errors coming from calls to Vulkan functions.
-    Vulkan(vk::Result),
+    #[error("A Vulkan error occured: {0}")]
+    Vulkan(#[from] vk::Result),
+
     /// Io errors.
-    Io(io::Error),
+    #[error("A io error occured: {0}")]
+    Io(#[from] std::io::Error),
+
     /// Initialization errors.
+    #[error("An error occured when initializing the renderer: {0}")]
     Init(String),
+
     /// Texture lookup error.
+    #[error("Bad texture ID: {}", .0.id())]
     BadTexture(TextureId),
-}
-
-impl fmt::Display for RendererError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use RendererError::*;
-        match self {
-            Vulkan(error) => write!(f, "A Vulkan error occured: {}", error),
-            Io(error) => write!(f, "An io error occured: {}", error),
-            Init(message) => write!(
-                f,
-                "An error occured when initializing the renderer: {}",
-                message
-            ),
-            BadTexture(texture_id) => write!(f, "Bad texture ID: {}", texture_id.id()),
-        }
-    }
-}
-
-impl Error for RendererError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        use RendererError::*;
-        match self {
-            Vulkan(error) => Some(error),
-            Io(error) => Some(error),
-            Init(..) | BadTexture(..) => None,
-        }
-    }
-}
-
-#[doc(hidden)]
-impl From<vk::Result> for RendererError {
-    fn from(error: vk::Result) -> RendererError {
-        RendererError::Vulkan(error)
-    }
-}
-
-#[doc(hidden)]
-impl From<io::Error> for RendererError {
-    fn from(error: io::Error) -> RendererError {
-        RendererError::Io(error)
-    }
 }
